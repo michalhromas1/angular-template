28ac639433210b4a6ad084a13ada6bf14edcc92a
Michal Hromas <michalhromas1@gmail.com>
2020-10-20 21:50:29 +0200

    Change app.module to core.module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,12 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './components/header/header.component';
 import { CoreComponent } from './core.component';
 import { CoreRoutingModule } from './router/core-routing.module';
-import { throwIfAlreadyLoaded } from './utils/module-import.guard';
 
 @NgModule({
   declarations: [CoreComponent, HeaderComponent],
   imports: [BrowserModule, CoreRoutingModule],
-  exports: [CoreComponent],
+  bootstrap: [CoreComponent],
 })
-export class CoreModule {
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
-    throwIfAlreadyLoaded(parentModule, 'CoreModule');
-  }
-}
+export class CoreModule {}
