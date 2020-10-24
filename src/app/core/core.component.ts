fc6fdb8942d84adbe29c76395f1af6e97e4a8f7b
Michal Hromas <michalhromas1@gmail.com>
2020-10-24 22:30:14 +0200

    Add default OnPush strategy

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './core.component.html',
   styleUrls: ['./core.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoreComponent {}
